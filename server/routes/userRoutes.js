3e3212545e2e16e848d66a1de8999a4aea4b823d
Yagrajoli <ekrajoli1235@gmail.com>
2025-05-17 13:42:28 +0545

    roled based auth

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,20 +1,21 @@
 const express = require("express");
-const protectRoute = require("../middlewares/authMiddlewares")
+const protectRoute = require("../middlewares/authMiddlewares");
+const verifyRole = require("../middlewares/roleMiddlewares");
 
 const router = express.Router();
 
 //only admin can access this route
-router.get("/admin", protectRoute, (req,res)=>{
+router.get("/admin", protectRoute, verifyRole("admin"), (req,res)=>{
     res.json({message:"welcome admin"})
 })
 
 //both admin and manager can access this route
-router.get("/manager",protectRoute, (req,res)=>{
+router.get("/manager",protectRoute, verifyRole("admin", "manager"), (req,res)=>{
     res.json({message:"welcome manager"})
 })
 
 //both user can access this route
-router.get("/user", protectRoute, (req,res)=>{
+router.get("/user", protectRoute, verifyRole("admin", "manager", "user"), (req,res)=>{
     res.json({message:"welcome user"})
 })
 module.exports = router;
